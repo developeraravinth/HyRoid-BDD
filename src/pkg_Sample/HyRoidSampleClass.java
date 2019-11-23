eeff8c801fbdbe8fbdce98cb6dfcfe32b18bbcb5
AravinthRaj <mailtoarvn@gmail.com>
2019-11-23 15:50:09 +0530

    Commit

---
+++ src/pkg_Sample/HyRoidSampleClass.java
@@ -0,0 +1,11 @@
+package pkg_Sample;
+
+public class HyRoidSampleClass {
+
+	static String name = null;
+	public static String HyRoidSampleMethod() {
+		name = "You have successfully hosted Mr. Aravinth. You are the most brilliant and has humungous grasping power. All the best!";
+		return name;
+	}
+
+}
--- src/pkg_Utilities/HyRoidUtilities.java
+++
@@ -1,9 +0,0 @@
-package pkg_Utilities;
-
-public class HyRoidUtilities {
-	
-	public static void myMessage() {
-		System.out.println("Aravinth is always Great chap!");
-	}
-
-}
