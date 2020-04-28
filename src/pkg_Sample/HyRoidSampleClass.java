c3c2fc83e8a77a3ad982683cd3245825b0f441cb
AravinthRaj <mailtoarvn@gmail.com>
2020-04-28 21:30:34 +0530

    Commit_Nexus_GIT

--- src/pkg_Sample/HyRoidSampleClass.java
+++ src/pkg_Sample/HyRoidSampleClass.java
@@ -4,6 +4,8 @@ public class HyRoidSampleClass {
 
 	private static String message = null;
 	private static String company = null;
+	private static String maruvarthai = null;
+	private static String yuvan = null;
 	
 	public static String HyRoidSampleMethod() {
 		message = "You have successfully hosted Mr. Aravinth. You are the most brilliant and has humungous grasping power. All the best!";
@@ -13,6 +15,28 @@ public class HyRoidSampleClass {
 	public static String CompanyName() {
 		company = "CGI Inc.,";
 		return company;
+	}
+	
+	public static String Dream() {
+		maruvarthai = "Maruvaarthai pesaadhey...! Madimeedhu nee thoongidu!!!";
+		return maruvarthai;
+	}
+	
+	public static String OwnRepo() {
+		yuvan = "Yuvan for Life";
+		return yuvan;
+	}
+	
+	public static String YuvanShankarRaja() {
+		return "I'll be there for you!!!";
+	}
+	
+	public static String version_2_0_4() {
+		return "Kannamoochi yenada!!";
+	}
+	
+	public static String version_2_0_5() {
+		return "Porkalam inge.. Poovin dhaagam enge?";
 	}
 
 }
